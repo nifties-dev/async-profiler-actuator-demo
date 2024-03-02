a5c6a65a050264e004008765dc30c6f31f14cd53
Andris Rauda <andris.rauda@substance.org.lv>
2024-03-02 12:53:26 +0200

    Initial generated web project

---
+++ src/main/java/dev/nifties/integration/boot/actuate/profiler/demo/AsyncProfilerActuatorDemoApplication.java
@@ -0,0 +1,13 @@
+package dev.nifties.integration.boot.actuate.profiler.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyncProfilerActuatorDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsyncProfilerActuatorDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/nifties/integration/boot/actuate/profiler/demo/AsyncProfilerActuatorDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.nifties.integration.boot.actuate.profiler.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsyncProfilerActuatorDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
