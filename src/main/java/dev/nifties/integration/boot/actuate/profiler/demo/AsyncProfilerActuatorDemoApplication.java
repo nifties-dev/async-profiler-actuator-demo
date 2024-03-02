2c1ec51132065d5fda6beec0838df6222e70186c
Andris Rauda <andris.rauda@substance.org.lv>
2024-03-02 22:20:52 +0200

    Adds AsyncProfilerActuator

--- src/main/java/dev/nifties/integration/boot/actuate/profiler/demo/AsyncProfilerActuatorDemoApplication.java
+++ src/main/java/dev/nifties/integration/boot/actuate/profiler/demo/AsyncProfilerActuatorDemoApplication.java
@@ -1,8 +1,10 @@
 package dev.nifties.integration.boot.actuate.profiler.demo;
 
+import dev.nifties.integration.springframework.boot.annotation.EnableAsyncProfiler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableAsyncProfiler
 @SpringBootApplication
 public class AsyncProfilerActuatorDemoApplication {
 
